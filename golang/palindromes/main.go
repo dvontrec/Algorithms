c4a285cc2b3e8382b005025762c9e9df7e348c7b
Dvontre Coleman <dvontrec@gmail.com>
2019-02-27 16:34:05 -0500

    Add palindrome check

--- golang/palindromes/main.go
+++ golang/palindromes/main.go
@@ -5,5 +5,10 @@ func main() {
 }
 
 func isPalindrome(s string) bool {
+	for i := range s {
+		if s[i] != s[(len(s)-1)-i] {
+			return false
+		}
+	}
 	return true
 }
--- golang/palindromes/palindrome_test.go
+++ golang/palindromes/palindrome_test.go
@@ -3,15 +3,15 @@ package main
 import "testing"
 
 func TestIsPalindrome(t *testing.T) {
-	if isPalindrome("abba") != false {
-		t.Errorf("Expected false, instead got %v", isPalindrome("abba"))
+	if isPalindrome("abba") != true {
+		t.Errorf("Expected true for abba, instead got %v", isPalindrome("abba"))
 	}
 	if isPalindrome("funny right?") != false {
-		t.Errorf("Expected false, instead got %v", isPalindrome("funnyright"))
+		t.Errorf("Expected false for funny right?, instead got %v", isPalindrome("funnyright"))
 	}
 
 	if isPalindrome("racecar") != true {
-		t.Errorf("Expected true, instead got %v", isPalindrome("racecar"))
+		t.Errorf("Expected true for racecar, instead got %v", isPalindrome("racecar"))
 	}
 
 }
